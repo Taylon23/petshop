86f6eb717e247a1e44f5f3681be533c9501db2ea
Taylon23 <taylonedits@gmail.com>
2024-12-17 16:27:24 -0300

    colocando anuncios

--- paginas/views.py
+++ paginas/views.py
@@ -1,20 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from animais import models
-
 
 def home(request):
     animais_destaques = models.Animal.objects.filter(destaque=True)
-    tipo_cachorro = models.TipoAnimal.objects.get(tipo_animal='Cachorro')
-    tipo_gato = models.TipoAnimal.objects.get(tipo_animal='Gato')
-    tipo_outros_animais = models.TipoAnimal.objects.get(
-        tipo_animal='Outro animal')
+    tipo_cachorro = get_object_or_404(models.TipoAnimal, tipo_animal='Cachorro')
+    tipo_gato = get_object_or_404(models.TipoAnimal, tipo_animal='Gato')
+    tipo_outros_animais = get_object_or_404(models.TipoAnimal, tipo_animal='Outro animal')
+    
     cachorros = models.Animal.objects.filter(animal=tipo_cachorro)
     gatos = models.Animal.objects.filter(animal=tipo_gato)
     outros_animais = models.Animal.objects.filter(animal=tipo_outros_animais)
-    return render(request, 'home.html', {'animais_destaques': animais_destaques, 
-                                         'cachorros': cachorros, 
-                                         'gatos': gatos, 'outros_animais': outros_animais})
-
-
-def base(request):
-    return render(request, 'base.html',)
+    
+    return render(request, 'home.html', {
+        'animais_destaques': animais_destaques, 
+        'cachorros': cachorros, 
+        'gatos': gatos, 
+        'outros_animais': outros_animais
+    })
